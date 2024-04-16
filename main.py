9abc5d20f3838a89a33b8504482f8d5ea554c8e6
jose rasgado <jrasgado01@hotmail.com>
2024-04-15 23:46:51 -0600

    Add Lines

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", size=24, style="B")
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 22, 200, 22)
-    pdf.ln(265)
+
+    for y in range(20, 297, 10):
+        pdf.line(10, y, 200, y)
 
     # Set the footer for the master page
+    pdf.ln(265)
     pdf.set_font(family="Times", size=8, style="I")
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
@@ -28,5 +30,8 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", size=8, style="I")
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for y in range(20, 297, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
