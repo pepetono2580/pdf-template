0921872e26fbcebcd06e1ec5c25c69f1bc690c9d
jose rasgado <jrasgado01@hotmail.com>
2024-04-15 23:31:16 -0600

    Add blank pages with footer

--- main.py
+++ main.py
@@ -2,18 +2,31 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
 
+    # Set the header
     pdf.set_font(family="Times", size=24, style="B")
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 22, 200, 22)
+    pdf.ln(265)
+
+    # Set the footer for the master page
+    pdf.set_font(family="Times", size=8, style="I")
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        # Set the footer for the other pages
+        pdf.ln(277)
+        pdf.set_font(family="Times", size=8, style="I")
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
